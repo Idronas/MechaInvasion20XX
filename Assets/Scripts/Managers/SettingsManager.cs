eebffcb5d5648da199ba8d9a6c38679887580b2b
Idronas <emichaelwatson@gmail.com>
2023-01-31 04:47:05 -0600

    Movement Base

---
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SettingsManager
+{
+   // Start is called before the first frame update
+   public static float lookSensitivity = 50f;
+}
